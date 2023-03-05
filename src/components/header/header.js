c25069753db7eeac051494fd4a71de8680d18791
BenB <boyer.benlee@gmail.com>
2023-03-05 14:47:46 -0800

    styled h1 text black

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ import Navbar from '../navbar/navbar';
 export default function Header({ routeInfo }) {
   return (
     <div className={`header row align-c collapse-stack`}>
-      <h1 className='header mx-s accent-b'>{routeInfo[0]}</h1>
+      <h1 className='header mx-s accent-b black'>{routeInfo[0]}</h1>
       <div>
         <hr className='header mx-s vr-xl' />
       </div>
