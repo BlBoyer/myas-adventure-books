6cf3974d9ff5955e1b8b72dcddff206752b3d348
BenB <boyer.benlee@gmail.com>
2023-03-05 16:22:43 -0800

    put bg image in all pages

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -4,9 +4,11 @@ import SectionHeader from '../../components/section-header/section-header';
 
 export default function About({ routeInfo }) {
   return (
-    <div id='about'>
-      <Header routeInfo={routeInfo} />
-      <SectionHeader routeInfo={routeInfo} />
+    <div id='main-div' className='about row'>
+      <div className='welcome col mx-vs my-xl bg-white faded-edge'>
+        <Header routeInfo={routeInfo} />
+        <SectionHeader routeInfo={routeInfo} />
+      </div>
     </div>
   );
 }
--- src/pages/author/author.js
+++ src/pages/author/author.js
@@ -4,9 +4,11 @@ import SectionHeader from '../../components/section-header/section-header';
 
 export default function Author({ routeInfo }) {
   return (
-    <div id='author'>
-      <Header routeInfo={routeInfo} />
-      <SectionHeader routeInfo={routeInfo} />
+    <div id='main-div' className='author row'>
+      <div className='welcome col mx-vs my-xl bg-white faded-edge'>
+        <Header routeInfo={routeInfo} />
+        <SectionHeader routeInfo={routeInfo} />
+      </div>
     </div>
   );
 }
