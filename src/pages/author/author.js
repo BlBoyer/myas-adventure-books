38319eba13e42589f74f6c2b6681a0cb7b7cafc9
BenB <boyer.benlee@gmail.com>
2023-03-06 19:31:44 -0800

    image work

--- src/pages/author/author.js
+++ src/pages/author/author.js
@@ -1,13 +1,20 @@
 import './author.css';
+import authorImg from '../../img/author.jpg';
 import Header from '../../components/header/header';
 import SectionHeader from '../../components/section-header/section-header';
 
 export default function Author({ routeInfo }) {
   return (
     <div id='main-div' className='author row'>
-      <div className='welcome col mx-vs my-xl bg-white faded-edge'>
+      <div className='author col mx-vs my-xl bg-white faded-edge'>
         <Header routeInfo={routeInfo} />
         <SectionHeader routeInfo={routeInfo} />
+        <div>
+          <hr />
+        </div>
+        <div className='author row justify-c mt-s'>
+          <img src={authorImg} className='author view-width v-40' />
+        </div>
       </div>
     </div>
   );
--- src/pages/illustrator/illustrator.js
+++ src/pages/illustrator/illustrator.js
@@ -1,13 +1,20 @@
 import './illustrator.css';
+import illustratorImg from '../../img/illustrator.jpg';
 import Header from '../../components/header/header';
 import SectionHeader from '../../components/section-header/section-header';
 
 export default function Illustrator({ routeInfo }) {
   return (
     <div id='main-div' className='illustrator row'>
-      <div className='welcome col mx-vs my-xl bg-white faded-edge'>
+      <div className='illustrator col mx-vs my-xl bg-white faded-edge'>
         <Header routeInfo={routeInfo} />
         <SectionHeader routeInfo={routeInfo} />
+        <div>
+          <hr />
+        </div>
+        <div class='illustrator row justify-c mt-s'>
+          <img src={illustratorImg} className='illustrator view-width v-40' />
+        </div>
       </div>
     </div>
   );
