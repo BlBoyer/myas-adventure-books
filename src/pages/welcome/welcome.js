c66f65f6caf6a3bf9421e080080bdcc4d972b46e
BenB <boyer.benlee@gmail.com>
2023-03-11 18:38:48 -0800

    created banner image for welcome page

--- src/pages/welcome/welcome.js
+++ src/pages/welcome/welcome.js
@@ -1,4 +1,5 @@
 import './welcome.css';
+import banner from '../../img/banner.webp';
 import Header from '../../components/header/header';
 import SectionHeader from '../../components/section-header/section-header';
 
@@ -7,6 +8,9 @@ export default function Welcome({ routeInfo }) {
     <div id='welcome' className='welcome row page-fade'>
       <div className='welcome col mx-vs my-xl bg-white faded-edge'>
         <Header routeInfo={routeInfo} />
+        <div className='welcome row justify-c my-l'>
+          <img src={banner} className='welcome view-width v-65 shaded-black' />
+        </div>
         <SectionHeader routeInfo={routeInfo} />
       </div>
     </div>
